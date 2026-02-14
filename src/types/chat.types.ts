0ec987ba5c5437a8de508ade175c5d167910096c
kush-ankit <er.ankitkush@gmail.com>
2026-02-14 09:57:45 +0530

    lobby added

--- src/types/chat.types.ts
+++
@@ -1,7 +0,0 @@
-import { Document } from "mongoose";
-
-export default interface IChat extends Document {
-    chat_id: string;
-    chat_members_user_id: string[];
-    createdAt: Date;
-}
--- src/types/message.types.ts
+++
@@ -1,6 +0,0 @@
-export default interface IMessage extends Document {
-    text_massage: string;
-    sender_user_id: string;
-    receiver_user_id: string;
-    created_at: Date;
-}
